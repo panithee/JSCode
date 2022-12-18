6d3bc38d9b37e2164a9bb78f51a54cee8f09d06d
panithee <panithee08@gmail.com>
2022-12-18 23:56:46 +0700

    last

--- test-paginate-panithee/paginateManagement.js
+++ test-paginate-panithee/paginateManagement.js
@@ -12,30 +12,34 @@ function paginateManagement(items, rows) {
   const products = items
 
   const showItemsOfCurrentPage = (currentPageNumber) => {
-    const showItem = getItemsOfCurrentPage(products, currentPageNumber, rowsPerPage) 
-    const ulElement = document.getElementById('products')
-    for(let i of showItem){
+    const showItem = getItemsOfCurrentPage(products, currentPageNumber, rowsPerPage)
+    const ulParent = document.getElementById('products')
+    showItem.forEach(item => {
       const liElement = document.createElement('li')
-      liElement.textContent = `ID:${i.id}, NAME:${i.name}`
-      ulElement.appendChild(liElement)
-    }
+      liElement.textContent = `ID:${item.id}, NAME:${item.name}`
+      ulParent.appendChild(liElement)
+    })   
 
   }
   const pageHandler = (event) => { 
-    const allButton = document.querySelectorAll('button')
-    allButton.forEach(page => page.style = "border: 1px solid #999")
-    const clearItem = document.getElementById('products')
-    clearItem.textContent = ""
+    // const selectButtonParent = document.querySelector('.pagination')
+    // const button = selectButtonParent.childNodes
+    const button = document.querySelectorAll('button')
+    button.forEach(page => page.style = 'border: 1px solid #999')
+
+    const clearText = document.querySelector('#products')
+    clearText.textContent = ""
+    
     const currentPage = event?.target.id ?? 1
     showItemsOfCurrentPage(currentPage)
-    const buttonOfCurrentPage = document.getElementById(currentPage)
-    buttonOfCurrentPage.style = "background-color: LightSteelBlue"
+    const currentButton = document.getElementById(currentPage)
+    currentButton.style = 'background-color: LightSteelBlue'
   }
 
   const showPageNumbers = () => {
     const totalPages = getTotalPages(products, rowsPerPage)
     const divClass = document.querySelector('.pagination')
-    for(let i=1; i<=totalPages ;i++){
+    for(let i=1; i<=totalPages; i++) {
       const button = document.createElement('button')
       button.setAttribute('id', i)
       button.textContent = i
