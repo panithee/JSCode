92aa69a6b5eb2abaadf5e9487968ef925127f054
panithee <panithee08@gmail.com>
2022-12-19 11:38:52 +0700

    abc

--- test-paginate-panithee/paginateManagement.js
+++ test-paginate-panithee/paginateManagement.js
@@ -12,8 +12,8 @@ function paginateManagement(items, rows) {
   const products = items
 
   const showItemsOfCurrentPage = (currentPageNumber) => {
-    const showItem = getItemsOfCurrentPage(products, currentPageNumber, rowsPerPage)
-    const ulParent = document.getElementById('products')
+    // const showItem = getItemsOfCurrentPage(products, currentPageNumber, rowsPerPage)
+    // const ulParent = document.getElementById('products')
     // showItem.forEach(item => {
     //   const liElement = document.createElement('li')
     //   liElement.textContent = `ID:${item.id}, NAME:${item.name}`
@@ -26,38 +26,71 @@ function paginateManagement(items, rows) {
     //   ulParent.appendChild(liElement)
     // } 
 
-    for(const item of showItem) {
+    // for(const item of showItem) {
+    //   const liElement = document.createElement('li')
+    //   liElement.textContent = `ID:${item.id}, NAME:${item.name}`
+    //   ulParent.appendChild(liElement)
+    // }
+
+    const showItemOnPage = getItemsOfCurrentPage(products, currentPageNumber, rowsPerPage)
+    const ulParent = document.getElementById('products')
+    showItemOnPage.forEach(item => {
       const liElement = document.createElement('li')
       liElement.textContent = `ID:${item.id}, NAME:${item.name}`
       ulParent.appendChild(liElement)
-    }
+    })
   }
   const pageHandler = (event) => { 
-    // const selectButtonParent = document.querySelector('.pagination')
-    // const button = selectButtonParent.childNodes
-    const button = document.querySelectorAll('button')
-    button.forEach(page => page.style = 'border: 1px solid #999')
-    // const button = document.getElementsByTagName('button')
-    // Array.from(button).forEach(page => page.style = 'border: 1px solid #999')
+    // // const selectButtonParent = document.querySelector('.pagination')
+    // // const button = selectButtonParent.childNodes
+    // const button = document.querySelectorAll('button')
+    // button.forEach(page => page.style = 'border: 1px solid #999')
+    // // const button = document.getElementsByTagName('button')
+    // // Array.from(button).forEach(page => page.style = 'border: 1px solid #999')
     
+    // const clearText = document.querySelector('#products')
+    // clearText.textContent = ""
+    
+    // const currentPage = event?.target.id ?? 1
+    // showItemsOfCurrentPage(currentPage)
+    // const currentButton = document.getElementById(currentPage)
+    // currentButton.style = 'background-color: LightSteelBlue'
+
+    const clearButtonStyle = document.querySelectorAll('button')
+    clearButtonStyle.forEach(page => page.style = 'border: 1px solid #999')
     const clearText = document.querySelector('#products')
-    clearText.textContent = ""
-    
-    const currentPage = event?.target.id ?? 1
+    clearText.textContent = ''
+    let currentPage
+    if(event === undefined || event === null){
+      currentPage = 1
+    }else{
+      currentPage = event.target.id
+    }
     showItemsOfCurrentPage(currentPage)
     const currentButton = document.getElementById(currentPage)
     currentButton.style = 'background-color: LightSteelBlue'
+
   }
 
   const showPageNumbers = () => {
-    const totalPages = getTotalPages(products, rowsPerPage)
+    // const totalPages = getTotalPages(products, rowsPerPage)
+    // const divClass = document.querySelector('.pagination')
+    // for(let i=1; i<=totalPages; i++) {
+    //   const button = document.createElement('button')
+    //   button.setAttribute('id', i)
+    //   button.textContent = i
+    //   divClass.appendChild(button)
+    //   button.addEventListener('click', pageHandler)
+    // }
+
+    const totalPage = getTotalPages(products, rowsPerPage)
     const divClass = document.querySelector('.pagination')
-    for(let i=1; i<=totalPages; i++) {
+    for(let i=1; i<=totalPage; i++) {
       const button = document.createElement('button')
       button.setAttribute('id', i)
       button.textContent = i
       divClass.appendChild(button)
-      button.addEventListener('click', pageHandler)
+      button.addEventListener('click',pageHandler)
     }
   } 
 
